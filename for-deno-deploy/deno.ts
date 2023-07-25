09d5f121cb5014f9d426a0f3be17f9afd136affc
Iya Rivvikyn <101973571+iyarivky@users.noreply.github.com>
2023-07-26 04:03:52 +0700

    Update deno.ts

--- for-deno-deploy/deno.ts
+++ for-deno-deploy/deno.ts
@@ -2,16 +2,19 @@ const html = `<!DOCTYPE html>
 <html>
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="https://raw.githubusercontent.com/iyarivky/sing-ribet-api/main/media/sing-ribet-convert.ico" rel="icon" type="image/x-icon">
+    <title>sing-ribet</title>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
       href="https://fonts.googleapis.com/css2?family=Rubik:wght@300&family=Source+Serif+4:wght@600&display=swap"
       rel="stylesheet"
     />
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/iyarivky/sing-ribet-web/css/styles.css" />
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/iyarivky/sing-ribet-web/css/js-snackbar.css" />
-    <script src="https://cdn.jsdelivr.net/gh/iyarivky/sing-ribet-web/js/script.js"></script>
-    <script src="https://cdn.jsdelivr.net/gh/iyarivky/sing-ribet-web/js//js-snackbar.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.iyariv.workers.dev/styles.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.iyariv.workers.dev/js-snackbar.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://cdn.iyariv.workers.dev/script.js"></script>
+    <script src="https://cdn.iyariv.workers.dev/js-snackbar.js"></script>
   </head>
   <body>
     <div class="judul">
@@ -30,7 +33,7 @@ const html = `<!DOCTYPE html>
           <button class="button" onclick="parseUrl()">Convert</button><br />
         </div>
         <h4><b>Base SFA Config</b></h4>
-        <textarea id="input"></textarea><br />
+        <textarea id="input-1"></textarea><br />
         <div class="tombol">
           <button class="button" onclick="downloadConfig()">Download</button>
           <button class="button" onclick="pasteConfig()">
@@ -38,7 +41,7 @@ const html = `<!DOCTYPE html>
           </button>
         </div>
         <h4><b>Simple SFA Config</b></h4>
-        <textarea id="input"></textarea><br />
+        <textarea id="input-2"></textarea><br />
         <div class="tombol">
           <button class="button" onclick="downloadConfig()">Download</button>
           <button class="button" onclick="pasteConfig()">
@@ -46,7 +49,7 @@ const html = `<!DOCTYPE html>
           </button>
         </div>
         <h4><b>Base BFM Config</b></h4>
-        <textarea id="input"></textarea><br />
+        <textarea id="input-3"></textarea><br />
         <div class="tombol">
           <button class="button" onclick="downloadConfig()">Download</button>
           <button class="button" onclick="pasteConfig()">
@@ -54,7 +57,7 @@ const html = `<!DOCTYPE html>
           </button>
         </div>
         <h4><b>Simple BFM Config</b></h4>
-        <textarea id="input"></textarea><br />
+        <textarea id="input-4"></textarea><br />
         <div class="tombol">
           <button class="button" onclick="downloadConfig()">Download</button>
           <button class="button" onclick="pasteConfig()">
@@ -65,7 +68,7 @@ const html = `<!DOCTYPE html>
     </div>
     <footer>
       <div class="kaki">
-        <p><a href="https://github.com/iyarivky/sing-ribet-web">Github</a></p>
+        <a href="https://github.com/iyarivky/sing-ribet-web" target="_blank"><i class="fa">&#xf09b;</i></a>
       </div>
     </footer>
   </body>
