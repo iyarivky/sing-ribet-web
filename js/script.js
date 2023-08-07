0a748fcb2ee90b3aa3f30bdc92b1e01f58cdd774
Iya Rivvikyn <101973571+iyarivky@users.noreply.github.com>
2023-08-07 11:45:56 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
-var pasteConfig = function () {
-  let def = SnackBar({
-    message: "Successfully copied to clipboard.",
-    position: "bc",
-    fixed: true,
-    status: "success"
-  });
-  var copyText = document.getElementById("output-1");
-  // Select the text field
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); // For mobile devices
-   // Copy the text inside the text field
-  navigator.clipboard.writeText(copyText.value);
+var pasteConfig = function(outputId) {
+	let def = SnackBar({
+		message: "Successfully copied to clipboard.",
+		position: "bc",
+		fixed: true,
+		status: "success"
+	});
+	var copyText = document.getElementById(outputId);
+	// Select the text field
+	copyText.select();
+	copyText.setSelectionRange(0, 99999); // For mobile devices
+	// Copy the text inside the text field
+	navigator.clipboard.writeText(copyText.value);
 };
 
 //parseUrl button
 function parseUrl() {
-    var inputText = document.getElementById("input").value;
-    document.getElementById("output-1").value = inputText;
-  }
+	var inputText = document.getElementById("input").value;
+	document.getElementById("output-1").value = inputText;
+	document.getElementById("output-2").value = inputText;
+	document.getElementById("output-3").value = inputText;
+	document.getElementById("output-4").value = inputText;
+}
