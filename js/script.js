904a01cc36bcb1b7c551fc671ce3fa04c9026f8e
Iya Rivvikyn <101973571+iyarivky@users.noreply.github.com>
2023-08-07 11:33:35 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,12 @@ var pasteConfig = function () {
     fixed: true,
     status: "success"
   });
+  var copyText = document.getElementById("output-1");
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
 };
 
 //parseUrl button
